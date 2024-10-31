f708599de6b8b3729c310e8e720dd4e0db55a73f
Taniya-Dutta <taniyadutta1001@gmail.com>
2024-10-31 18:12:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,15 +3,11 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 import tensorflow as tf
-from tensorflow.keras.preprocessing.sequence import pad_sequences  # Updated import
+from keras_preprocessing.sequence import pad_sequences
 app = Flask(__name__)
-
 # Load the trained model and tokenizer
-try:
-    with open('bidirectional_LSTM.pkl', 'rb') as model_file:
-        model = pickle.load(model_file)
-except Exception as e:
-    print(f"Error loading model: {e}")
+with open('bidirectional_LSTM.pkl', 'rb') as model_file:
+    model = pickle.load(model_file)
 
 with open('tokenizer.pkl', 'rb') as tokenizer_file:
     tokenizer = pickle.load(tokenizer_file)
@@ -32,10 +28,9 @@ def predict():
         # Predict sentiment (0 or 1)
         prediction = model.predict(padded_sequence)
         sentiment = 'Positive' if prediction[0][0] >= 0.3 else 'Negative'
-        sentiment_score = prediction[0][0]  # Display the raw score
+        sentiment_score = prediction[0][0]  # Assuming you want to display the raw score
         return render_template('result.html', review=review, sentiment=sentiment, sentiment_score=sentiment_score)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
 
 
